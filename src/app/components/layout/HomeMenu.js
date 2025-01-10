68cac806da2ecb2a18d9bfab0ba3b788674c9597
Avok@to34 <vladlen-17@yandex.ru>
2025-01-10 23:57:03 +0300

    feat: made some changes in HomeMenu

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,8 +1,30 @@
+import Image from "next/image";
+
 export default function HomeMenu() {
   return (
-    <div className="">
-      <h3 className="text-center">Check out</h3>
-      <h2 className="text-primary font-bold text-4xl">Menu</h2>
-    </div>
+    <section className="h-48 relative">
+      <div>
+        <Image
+          src={"/salad1.png"}
+          layout={"fill"}
+          objectFit="contain"
+          alt="salad"
+        />
+      </div>
+      <div>
+        <Image
+          src={"/salad2.png"}
+          layout={"fill"}
+          objectFit="contain"
+          alt="salad"
+        />
+      </div>
+      <div className="text-center">
+        <h3 className="uppercase text-gray-500 font-semibold leading-4">
+          Check out
+        </h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+      </div>
+    </section>
   );
 }
