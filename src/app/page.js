87f2d122bd8a6ef306af4af1ab2ca7a8c1200c5a
Avok@to34 <vladlen-17@yandex.ru>
2025-01-15 00:17:06 +0300

    feat: created SectionHeader component and ended HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,50 @@
 import Header from "./components/layout/Header";
 import Hero from "./components/layout/Hero";
 import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Hero/>
-      <HomeMenu/>
+      <Hero />
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-2xl mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          
+        </div>
+        <a className="text-4xl underline text-gray-500" href="tel:+46738123123">
+          +46 738 123 123
+        </a>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+    &copy; 2025 All rights reserved
+      </footer>
     </>
   );
 }
